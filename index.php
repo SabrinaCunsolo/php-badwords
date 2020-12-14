61eba97603372563393f3cf7ba1062c0314ec812
Sabrina <sabrinacunsolo85@gmail.com>
2020-12-14 16:37:47 +0100

    inizio esercizio

---
+++ index.php
@@ -0,0 +1,33 @@
+<!-- Create una variabile con un paragrafo.
+Leggete da un parametro GET della querystring una "badword"
+Sostituite all'interno del paragrafo tutte le occorrenze della "badword" con *** (3 asterischi).
+Stampate a schermo il paragrafo censurato e la lunghezza del paragrafo originale.
+Nome repo: php-badwords -->
+
+<?php
+//paragrafo
+$testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+$lunghezza_testo = strlen($testo); //variabile per lunghezza paragrafo
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Bad Words</title>
+    </head>
+    <body>
+        <p>
+            <?php
+
+            ?>
+        </p>
+
+
+    </body>
+</html>
